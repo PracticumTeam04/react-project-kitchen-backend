c8bb760547cb8b961c628ab0d6c2973d08ab99b7
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-12-20 13:41:31 +0300

    ref: replace fs sync funcs with promises

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,15 +1,14 @@
 const crypto = require('crypto');
-const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
-const {
-  HOST,
-} = require('../config');
+const { HOST } = require('../config');
 const { articleImageUpload } = require('../utils/article-image-uploader');
-const UPLOAD_FILE_FOLDER = process.env.UPLOAD_FILE_FOLDER || '/usr/src/app/public/uploads';
+const UPLOAD_FILE_FOLDER =
+  process.env.UPLOAD_FILE_FOLDER || '/usr/src/app/public/uploads';
 const ARTICLE_IMAGE_FIELDNAME = 'article-image';
 
 const createHash = (req, res, next) => {
-  if(!req.file) {
+  if (!req.file) {
     return next();
   }
   try {
@@ -25,28 +24,37 @@ const createHash = (req, res, next) => {
 };
 
 const writeFile = (req, res, next) => {
-  if(!req.file) {
+  if (!req.file) {
     return next();
   }
-  try {
-    const fileName =
-      res.locals.fileHash + path.extname(req.file.originalname).toLowerCase();
-    delete res.locals.fileHash;
-    const uploadPath = UPLOAD_FILE_FOLDER;
-    res.locals.fileUrl = `${HOST}/uploads/${fileName}`;
-    const files = fs.readdirSync(uploadPath);
-    if (files.includes(fileName)) {
-      delete req.file.buffer;
-      next();
-    } else {
-      fs.writeFileSync(`${uploadPath}/${fileName}`, req.file.buffer, 'binary');
-      // TODO: clear buffer?
-      delete req.file.buffer;
-      next();
-    }
-  } catch (error) {
-    next(error);
-  }
+
+  const fileName =
+    res.locals.fileHash + path.extname(req.file.originalname).toLowerCase();
+  delete res.locals.fileHash;
+  const uploadPath = UPLOAD_FILE_FOLDER;
+  res.locals.fileUrl = `${HOST}/uploads/${fileName}`;
+  fsPromises
+    .readdir(uploadPath)
+    .then((files) => {
+      if (files.includes(fileName)) {
+        delete req.file.buffer;
+        next();
+      } else {
+        fsPromises
+          .writeFile(`${uploadPath}/${fileName}`, req.file.buffer, 'binary')
+          .then(() => {
+            delete req.file.buffer;
+            next();
+          })
+          .catch((error) => {
+            delete req.file.buffer;
+            throw error;
+          });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 module.exports.saveArticleImage = [
